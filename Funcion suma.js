c7d231aa06bcc0e0fdfc27a22310fe964753349b
Paula Schuppe <107902047+Paulas12@users.noreply.github.com>
2023-02-15 17:00:44 -0300

    Update Funcion suma.js

--- Funcion suma.js
+++ Funcion suma.js
@@ -3,7 +3,7 @@ function suma(a,b){
     console.log("Mensaje dentro de la funcion");
     console.log(`El resultado será: ${a+b}`);
     return a+b;
-    console.log("este mensaje será ignordo");
+    console.log("este mensaje será ignorado");
 }
 
 let numA = 5;
@@ -12,5 +12,5 @@ let numB = 10;
 //invocacion o llamado 
 let resultado = suma(numA,numB);
 console.log("Mensaje fuera de la funcion");
-console.log(`El valor que devolvió la funcion será: ${resultado}`);
+console.log(`El valor que devolvió la funcion fue: ${resultado}`);
 console.log(`El valor que devolvió la funcion es: ${suma(numA,numB)}`); 
