f40fca11e6801bab9e5cd72e8703ae41917c2e28
Paula Schuppe <107902047+Paulas12@users.noreply.github.com>
2023-02-23 21:41:13 -0300

    Update Ambito de variables-ejercicio5.js

--- Ambito de variables-ejercicio5.js
+++ Ambito de variables-ejercicio5.js
@@ -2,8 +2,9 @@ var nuevo_numero = 5;
 
 function tablaNumero(nuevo_numero) {
     for (var i = 1; i <= 10; i++) {
-        resultado = nuevo_numero * i;
-        console.log(resultado);
+        //resultado = nuevo_numero * i;
+        //console.log(resultado);
+        console.log(nuevo_numero * i);
     }
     return nuevo_numero*10;
 }
